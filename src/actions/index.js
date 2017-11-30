cefc2a3f5c27f3d92cf62f0512f3b3c13f221526
akari0624 <akari0624@hotmail.com>
2017-11-30 10:40:06 +0800

    use axios to get fake user data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 import { FETCH_USERS } from "./type";
 
+
+const fakeJsonDataUrl = 'https://jsonplaceholder.typicode.com/users';
+
 export function fetchUsers() {
+
+    const response = axios.get(fakeJsonDataUrl);
+
   return {
     type: FETCH_USERS,
-    payload: [{ name: "Jane" }, { name: "Alex" }, { name: "Jim" }]
+    payload: response
   };
 }
